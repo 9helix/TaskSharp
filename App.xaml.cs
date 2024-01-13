90ff2d45c7bb3df86da13f41479e70f33ac2533c
Dino Gržinić <grzinicdino@gmail.com>
2024-01-13 21:24:26 +0100

    Added login page

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
 namespace TaskSharp
 {
@@ -9,6 +7,16 @@ namespace TaskSharp
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+            // Create the startup window
+            MainWindow wnd = new MainWindow();
+            // Do stuff here, e.g. to the window
+            wnd.Title = "Something else";
+            // Show the window
+            wnd.Show();
+        }
     }
 
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TaskSharp
 {
@@ -20,5 +12,27 @@ namespace TaskSharp
         {
             InitializeComponent();
         }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            //ystem.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string user = txtUser.Text;
+            if (user.Length < 2)
+                MessageBox.Show("Korisničko ime mora imati bar 2 znaka.", "Greška prijave", MessageBoxButton.OK, MessageBoxImage.Warning);
+            string pass = txtPass.Password;
+            if (pass.Length < 4)
+                MessageBox.Show("Lozinka mora sadržavati bar 4 znaka.", "Greška prijave", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+
     }
 }
