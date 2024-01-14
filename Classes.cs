325d7721cbbc67078ccfa61c11d136d7fb727b50
Marin <Marin@DESKTOP-N4EAFK4>
2024-01-14 11:57:05 +0100

    Added files for classes and DbContext derived class

---
+++ Classes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSharp
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        // reference on user's notes
+        public virtual ICollection<BaseNote> TaskList
+            { get; private set;  } = new ObservableCollection<BaseNote>();
+    }
+
+    public abstract class BaseNote
+    {
+        public int BaseNoteId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Name { get; set; }
+        public List<string> Tags { get; set; }
+        public bool Pinned { get; set; }
+
+        // foreign key on User
+        public int UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+
+    public class Note : BaseNote
+    {
+        public string Content { get; set; }
+    }
+
+    public class Event : BaseNote
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Location { get; set; }
+    }
+
+    public class Reminder : BaseNote
+    {
+        public DateTime DueDate { get; set; }
+        public enum ReminderPriority
+        {
+            Low, Medium, High
+        }
+    }
+
+    public class TodoList : BaseNote
+    {
+        public Dictionary<string, bool> Todos { get; set; }
+    }
+}
---
+++ NotesContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSharp
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<BaseNote> BaseNotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=notes.db");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}
