5993d52c6213b77d5791266deae2454b16a103d6
Dino Gržinić <grzinicdino@gmail.com>
2024-01-16 00:03:57 +0100

    Added note creation window

--- NoteCreate.xaml.cs
+++ NoteCreate.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace TaskSharp
 {
@@ -22,6 +12,93 @@ namespace TaskSharp
         public NoteCreate()
         {
             InitializeComponent();
+            EventStartPick.BlackoutDates.AddDatesInPast();
+            EventStartPick.SelectedDate = DateTime.Now;
+            EventEndPick.BlackoutDates.AddDatesInPast();
+            EventEndPick.SelectedDate = DateTime.Now;
+            ReminderDuePick.BlackoutDates.AddDatesInPast();
+            ReminderDuePick.SelectedDate = DateTime.Now;
+            NoteContent.Visibility = Visibility.Visible;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Debug.WriteLine("hi");
+
+            int index = NoteTypeMenu.SelectedIndex;
+            string type = "";
+            switch (index)
+            {
+                case 0:
+                    type = "Bilješka";
+
+                    break;
+                case 1:
+                    type = "Događaj";
+
+                    break;
+                case 2:
+                    type = "Podsjetnik";
+
+                    break;
+                case 3:
+                    type = "ToDo";
+
+                    break;
+            }
+            if (NoteContent != null)
+            {
+                ToggleFields(type);
+                //test.Children.Add(new TextBox { });
+            }
+            Debug.WriteLine(type);
+        }
+        private void ToggleFields(string type)
+        {
+            if (type != "Bilješka")
+            {
+                NoteContent.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                NoteContent.Visibility = Visibility.Visible;
+            }
+            if (type != "Događaj")
+            {
+                EventStart.Visibility = Visibility.Collapsed;
+                EventEnd.Visibility = Visibility.Collapsed;
+                txtLocation.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                EventStart.Visibility = Visibility.Visible;
+                EventEnd.Visibility = Visibility.Visible;
+                txtLocation.Visibility = Visibility.Visible;
+            }
+            if (type != "Podsjetnik")
+            {
+                ReminderDue.Visibility = Visibility.Collapsed;
+                PriorityMenu.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                ReminderDue.Visibility = Visibility.Visible;
+                PriorityMenu.Visibility = Visibility.Visible;
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddTodo(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Todos.Children.Add(new TextBox { }); //continue here
         }
     }
 }
